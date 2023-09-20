9e155a7907728cfc5922de80ae6c235bd4a0bced
DaniloSalerno <salerno.danilo@hotmail.it>
2023-09-20 14:32:01 +0200

    generate 10 random email address and print in html page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,40 @@
+/*
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+
 const { createApp } = Vue
 
-  createApp({
-    data() {
-      return {
-      }
+createApp({
+  data() {
+    return {
 
-    },
-    methods: {    
-    },
-    mounted() {
-      console.log('hello');
+      listEmail: []
+      
     }
-    
-  }).mount('#app')
 
-  
+  },
+  methods: {
+
+  },
+  mounted() {
+
+    for (let i = 0; i < 10; i++) {
+
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+
+          this.listEmail.push(response.data.response)
+
+        })
+
+    }
+
+
+    console.log(this.listEmail);
+  }
+
+}).mount('#app')
+
