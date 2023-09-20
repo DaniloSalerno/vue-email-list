9f3193c33ce02c550e8b92b81d8e9a0f9e48feeb
DaniloSalerno <salerno.danilo@hotmail.it>
2023-09-20 14:19:41 +0200

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+      }
+
+    },
+    methods: {    
+    },
+    mounted() {
+      console.log('hello');
+    }
+    
+  }).mount('#app')
+
+  
